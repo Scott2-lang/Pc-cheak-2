6a92a9689a48bed875fa3c95a2c7f85c43b7f646
Scott2-lang <ishakyazid876@gmail.com>
2026-09-06 22:03:50 +0100

    Fix duplicate assignment of PC_CHEAK_API

--- github-pages/config.js
+++ github-pages/config.js
@@ -1,2 +1,2 @@
 // Public API URL used by the GitHub Pages Staff UI.
-window.PC_CHEAK_API = window.PC_CHEAK_API = 'https://pc-cheak-2.onrender.com';
+window.PC_CHEAK_API = 'https://pc-cheak-2.onrender.com';
